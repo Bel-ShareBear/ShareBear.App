e9435d19e3c428f9330ee7408a0130f8950c7938
Nguyen, Le <lnguyen@illumina.com>
2016-06-02 00:46:58 -0700

    Initial Commit

---
+++ Kentro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kentro
+//
+//  Created by Nguyen, Le on 6/2/16.
+//  Copyright © 2016 Nguyen, Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
